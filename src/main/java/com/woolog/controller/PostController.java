373909dbbede3d9cdae54ff3c15615b8c95d28d4
박관우 <pgw4712@icloud.com>
2024-11-19 14:42:20 +0900

    updated .gitignore for nodeenv

--- src/main/java/com/woolog/controller/PostController.java
+++ src/main/java/com/woolog/controller/PostController.java
@@ -8,14 +8,9 @@ import com.woolog.service.PostService;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.data.domain.Pageable;
-import org.springframework.data.domain.Sort;
-import org.springframework.data.web.PageableDefault;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
-
-import static org.springframework.data.domain.Sort.Direction.DESC;
 
 @RestController
 @Slf4j
