17b852301159d91b23ce66fb0ffb89e9fb2c23da
박관우 <pgw4712@icloud.com>
2024-12-20 22:23:44 +0900

    예외처리 중앙 집중화

--- src/main/java/com/woolog/controller/WoologExceptionController.java
+++ src/main/java/com/woolog/controller/WoologExceptionController.java
@@ -1,11 +1,12 @@
 package com.woolog.controller;
 
-import com.woolog.exception.SecurityException;
+import com.woolog.exception.CustomException;
 import com.woolog.exception.WoologException;
 import com.woolog.response.CommonResponse;
 import com.woolog.response.CommonResponseField;
+import io.jsonwebtoken.JwtException;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.AccessDeniedException;
 import org.springframework.security.core.AuthenticationException;
 import org.springframework.validation.FieldError;
 import org.springframework.web.bind.MethodArgumentNotValidException;
@@ -21,7 +22,7 @@ import static com.woolog.response.ResponseStatus.BAD_REQUEST;
 public class WoologExceptionController {
 
     @ExceptionHandler(MethodArgumentNotValidException.class)
-    public ResponseEntity<CommonResponse<CommonResponseField>> validExceptionHandler(MethodArgumentNotValidException e) {
+    public CommonResponse<CommonResponseField> validExceptionHandler(MethodArgumentNotValidException e) {
 
         CommonResponse<CommonResponseField> commonResponse = new CommonResponse<>(BAD_REQUEST);
         List<FieldError> fieldErrors = e.getFieldErrors();
@@ -30,27 +31,15 @@ public class WoologExceptionController {
             commonResponse.addData(new CommonResponseField(fieldError.getField(), fieldError.getDefaultMessage()));
         }
 
-        return ResponseEntity.status(commonResponse.getStatus())
-                .body(commonResponse);
+        return commonResponse;
     }
 
-    @ExceptionHandler(WoologException.class)
-    public ResponseEntity<CommonResponse<CommonResponseField>> woologExceptionHandler(WoologException e) {
+    @ExceptionHandler(value = {WoologException.class, AuthenticationException.class, JwtException.class, AccessDeniedException.class})
+    public CommonResponse<CommonResponseField> woologExceptionHandler(CustomException e) {
 
         CommonResponse<CommonResponseField> commonResponse = new CommonResponse<>(e.getHttpStatus());
         commonResponse.addData(e.getErrorResponse());
 
-        return ResponseEntity.status(commonResponse.getStatus())
-                .body(commonResponse);
-    }
-
-    @ExceptionHandler(SecurityException.class)
-    public ResponseEntity<CommonResponse<CommonResponseField>> testHandler(SecurityException e) {
-
-        CommonResponse<CommonResponseField> commonResponse = new CommonResponse<>(e.getHttpStatus());
-        commonResponse.addData(e.getErrorResponse());
-
-        return ResponseEntity.status(commonResponse.getStatus())
-                .body(commonResponse);
+        return commonResponse;
     }
 }
