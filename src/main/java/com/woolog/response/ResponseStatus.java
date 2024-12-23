66fb65f45354bada3c5b8a01d29813919b6dd507
박관우 <pgw4712@icloud.com>
2024-12-23 22:17:50 +0900

    code 변수 삭제 및 description -> message 변경

--- src/main/java/com/woolog/response/ResponseStatus.java
+++ src/main/java/com/woolog/response/ResponseStatus.java
@@ -9,26 +9,24 @@ public enum ResponseStatus {
 
     /**
      * status : int
-     * code: String
-     * description: String
+     * message: String
      */
 
     // HTTP STATUS
-    BAD_REQUEST(400, "BAD_REQUEST", "잘못된 입력 값입니다."),
-    NOT_FOUND(404, "NOT_FOUND", "잘못된 요청입니다."),
-    INTERNAL_SERVER_ERROR(500, "INTERNAL_SERVER_ERROR", "서버에러"),
+    BAD_REQUEST(400, "잘못된 입력 값입니다."),
+    NOT_FOUND(404, "잘못된 요청입니다."),
+    INTERNAL_SERVER_ERROR(500, "서버에러"),
 
     // CUSTOM STATUS
-    MEMBER_AUTHENTICATION_EXCEPTION(401, "MEMBER_AUTHENTICATION_EXCEPTION", "잘못된 회원정보입니다."),
-    DUPLICATE_EMAIL_EXCEPTION(401,"DUPLICATE_EMAIL_EXCEPTION","이미 존재하는 이메일입니다."),
-    DUPLICATE_NICKNAME_EXCEPTION(401,"DUPLICATE_NICKNAME_EXCEPTION","이미 존재하는 닉네임입니다."),
-    MEMBER_NOT_EXIST(401,"MEMBER_NOT_EXIST","존재하지 않는 회원입니다."),
-    JWT_VERIFY_EXCEPTION(401,"JWT_VERIFY_EXCEPTION","유효하지 않은 토큰입니다."),
-    AUTHORIZE_EXCEPTION(401,"AUTHORIZE_EXCEPTION","접근 권한이 없습니다.")
+    MEMBER_AUTHENTICATION_EXCEPTION(401, "잘못된 회원정보입니다."),
+    DUPLICATE_EMAIL_EXCEPTION(401,"이미 존재하는 이메일입니다."),
+    DUPLICATE_NICKNAME_EXCEPTION(401,"이미 존재하는 닉네임입니다."),
+    MEMBER_NOT_EXIST(401,"존재하지 않는 회원입니다."),
+    JWT_VERIFY_EXCEPTION(401,"유효하지 않은 토큰입니다."),
+    AUTHORIZE_EXCEPTION(401,"접근 권한이 없습니다.")
     ;
 
     // WOOLOG ERROR CODE
     private final int status;
-    private final String code;
     private final String message;
 }
