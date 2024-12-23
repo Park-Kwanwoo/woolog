d14c71e3d19941a2234e2650a6c857dfa6678a64
박관우 <pgw4712@icloud.com>
2024-12-23 22:23:14 +0900

    회원 비즈니스 로직 테스트 코드

--- src/test/java/com/woolog/controller/MemberControllerTest.java
+++ src/test/java/com/woolog/controller/MemberControllerTest.java
@@ -1,27 +1,34 @@
 package com.woolog.controller;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.woolog.config.HashEncrypt;
+import com.woolog.config.JwtTokenGenerator;
 import com.woolog.domain.Member;
 import com.woolog.domain.Role;
 import com.woolog.exception.DuplicateEmailException;
 import com.woolog.exception.DuplicateNickNameException;
+import com.woolog.exception.MemberNotExist;
 import com.woolog.repository.MemberRepository;
+import com.woolog.request.MemberEdit;
 import com.woolog.request.Signup;
 import com.woolog.response.ResponseStatus;
 import com.woolog.service.MemberService;
+import jakarta.servlet.http.Cookie;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.HttpHeaders;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 
 import static org.junit.jupiter.api.Assertions.*;
 import static org.springframework.http.MediaType.APPLICATION_JSON;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
@@ -43,6 +50,30 @@ class MemberControllerTest {
     @Autowired
     private ObjectMapper objectMapper;
 
+    @Autowired
+    private JwtTokenGenerator jwtTokenGenerator;
+
+    @Autowired
+    private HashEncrypt hashEncrypt;
+
+    @BeforeEach
+    void setUp() {
+        memberRepository.deleteAll();
+    }
+
+    private Member memberCreate() {
+        Member member = Member.builder()
+                .email("member@blog.com")
+                .name("선점박")
+                .password("qwer123$")
+                .nickName("선점박")
+                .hashId(hashEncrypt.encrypt("member@blog.com"))
+                .role(Role.MEMBER)
+                .build();
+
+        return memberRepository.save(member);
+    }
+
     @Nested
     @DisplayName("성공 케이스")
     class successCase {
@@ -53,7 +84,7 @@ class MemberControllerTest {
 
             // given
             Signup signup = Signup.builder()
-                    .email("test@blog.com")
+                    .email("member@blog.com")
                     .password("qwer123$")
                     .name("테스터박")
                     .nickName("테스터박")
@@ -68,6 +99,92 @@ class MemberControllerTest {
                     .andExpect(status().isOk())
                     .andDo(print());
 
+        }
+
+        @Test
+        @DisplayName("회원정보 가져오기")
+        void GET_MEMBER_INFO() throws Exception {
+
+            // given
+            Member member = memberCreate();
+            String memberHashId = member.getHashId();
+
+            String accessToken = jwtTokenGenerator.generateAccessToken("member@blog.com");
+            String refreshToken = jwtTokenGenerator.generateRefreshToken("member@blog.com");
+            HttpHeaders headers = new HttpHeaders();
+            headers.add("Authorization", accessToken);
+            Cookie cookie = new Cookie("refreshToken", refreshToken);
+
+            // expected
+            mockMvc.perform(get("/members/{memberHashId}", memberHashId)
+                            .contentType(APPLICATION_JSON)
+                            .headers(headers)
+                            .cookie(cookie))
+                    .andExpect(jsonPath("$.email").value(member.getEmail()))
+                    .andExpect(jsonPath("$.name").value(member.getName()))
+                    .andExpect(jsonPath("$.nickName").value(member.getNickName()))
+                    .andDo(print());
+
+        }
+
+        @Test
+        @DisplayName("회원정보 수정")
+        void EDIT_MEMBER_INFO() throws Exception {
+
+            // given
+            Member member = memberCreate();
+            String memberHashId = member.getHashId();
+
+            String accessToken = jwtTokenGenerator.generateAccessToken("member@blog.com");
+            String refreshToken = jwtTokenGenerator.generateRefreshToken("member@blog.com");
+            HttpHeaders headers = new HttpHeaders();
+            headers.add("Authorization", accessToken);
+            Cookie cookie = new Cookie("refreshToken", refreshToken);
+
+            MemberEdit memberEdit = MemberEdit.builder()
+                    .nickName("닉네임 변경")
+                    .password("qwer123$")
+                    .build();
+
+            String editRequest = objectMapper.writeValueAsString(memberEdit);
+
+            // expected
+            mockMvc.perform(patch("/members/{memberHashId}", memberHashId)
+                            .contentType(APPLICATION_JSON)
+                            .content(editRequest)
+                            .headers(headers)
+                            .cookie(cookie))
+                    .andDo(print());
+
+            Member editMember = memberRepository.findMemberByEmail(member.getEmail())
+                    .orElseThrow(() -> new MemberNotExist("member", "존재하지 않는 이메일입니다."));
+
+            assertEquals("닉네임 변경", editMember.getNickName());
+
+        }
+
+        @Test
+        @DisplayName("회원탈퇴")
+        void DELETE_MEMBER_INFO() throws Exception {
+
+            // given
+            Member member = memberCreate();
+            String memberHashId = member.getHashId();
+
+            String accessToken = jwtTokenGenerator.generateAccessToken("member@blog.com");
+            String refreshToken = jwtTokenGenerator.generateRefreshToken("member@blog.com");
+            HttpHeaders headers = new HttpHeaders();
+            headers.add("Authorization", accessToken);
+            Cookie cookie = new Cookie("refreshToken", refreshToken);
+
+            // expected
+            mockMvc.perform(delete("/members/{memberHashId}", memberHashId)
+                            .contentType(APPLICATION_JSON)
+                            .headers(headers)
+                            .cookie(cookie))
+                    .andDo(print());
+
+            assertThrows(MemberNotExist.class, () -> memberService.getMember(memberHashId));
         }
     }
 
@@ -88,7 +205,6 @@ class MemberControllerTest {
                             .content(json)
                             .contentType(APPLICATION_JSON))
                     .andExpect(jsonPath("$.status").value(ResponseStatus.BAD_REQUEST.getStatus()))
-                    .andExpect(jsonPath("$.code").value(ResponseStatus.BAD_REQUEST.getCode()))
                     .andExpect(jsonPath("$.message").value(ResponseStatus.BAD_REQUEST.getMessage()))
                     .andExpect(result ->
                             assertInstanceOf(MethodArgumentNotValidException.class, result.getResolvedException()))
@@ -101,16 +217,17 @@ class MemberControllerTest {
 
             // given
             Member member = Member.builder()
-                    .email("test@blog.com")
+                    .email("member@blog.com")
                     .name("선점박")
                     .password("qwer123$")
                     .nickName("선점박")
+                    .hashId(hashEncrypt.encrypt("member@blog.com"))
                     .role(Role.MEMBER)
                     .build();
             memberRepository.save(member);
 
             Signup signup = Signup.builder()
-                    .email("test@blog.com")
+                    .email("member@blog.com")
                     .name("테스터박")
                     .password("qwer123$")
                     .nickName("테스터박")
@@ -123,7 +240,6 @@ class MemberControllerTest {
                             .content(json)
                             .contentType(APPLICATION_JSON))
                     .andExpect(jsonPath("$.status").value(ResponseStatus.DUPLICATE_EMAIL_EXCEPTION.getStatus()))
-                    .andExpect(jsonPath("$.code").value(ResponseStatus.DUPLICATE_EMAIL_EXCEPTION.getCode()))
                     .andExpect(jsonPath("$.message").value(ResponseStatus.DUPLICATE_EMAIL_EXCEPTION.getMessage()))
                     .andExpect(jsonPath("$.data[0].field").value("email"))
                     .andExpect(jsonPath("$.data[0].message").value("이미 존재하는 이메일입니다."))
@@ -142,6 +258,7 @@ class MemberControllerTest {
                     .name("선점닉")
                     .password("qwer123$")
                     .nickName("선점닉")
+                    .hashId(hashEncrypt.encrypt("test1@blog.com"))
                     .role(Role.MEMBER)
                     .build();
             memberRepository.save(member);
@@ -160,7 +277,6 @@ class MemberControllerTest {
                             .content(json)
                             .contentType(APPLICATION_JSON))
                     .andExpect(jsonPath("$.status").value(ResponseStatus.DUPLICATE_NICKNAME_EXCEPTION.getStatus()))
-                    .andExpect(jsonPath("$.code").value(ResponseStatus.DUPLICATE_NICKNAME_EXCEPTION.getCode()))
                     .andExpect(jsonPath("$.message").value(ResponseStatus.DUPLICATE_NICKNAME_EXCEPTION.getMessage()))
                     .andExpect(jsonPath("$.data[0].field").value("nickname"))
                     .andExpect(jsonPath("$.data[0].message").value("이미 존재하는 닉네임입니다."))
