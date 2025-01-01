c3f980c831749d5f8ab1754d99f5e0c3e4689554
박관우 <pgw4712@icloud.com>
2025-01-01 23:20:00 +0900

    회원 정보 추가

--- src/main/java/com/woolog/response/PostResponse.java
+++ src/main/java/com/woolog/response/PostResponse.java
@@ -10,18 +10,21 @@ public class PostResponse {
     private final Long id;
     private final String title;
     private final String content;
+    private final String nickname;
 
     public PostResponse(Post post) {
         this.id = post.getId();
         this.title = post.getTitle();
         this.content = post.getContent();
+        this.nickname = post.getMember().getNickName();
     }
 
     @Builder
-    public PostResponse(Long id, String title, String content) {
+    public PostResponse(Long id, String title, String content, String nickname) {
         this.id = id;
         this.title = title;
         this.content = content;
+        this.nickname = nickname;
     }
 
 }
