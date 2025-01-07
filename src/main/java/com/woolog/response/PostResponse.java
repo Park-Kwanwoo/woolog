3f6c6b89e40f458503d739aca366ab2f61e9bcc4
박관우 <pgw4712@icloud.com>
2025-01-07 21:31:39 +0900

    응답 필드 추가

--- src/main/java/com/woolog/response/PostResponse.java
+++ src/main/java/com/woolog/response/PostResponse.java
@@ -4,6 +4,8 @@ import com.woolog.domain.Post;
 import lombok.Builder;
 import lombok.Getter;
 
+import java.time.LocalDateTime;
+
 @Getter
 public class PostResponse {
 
@@ -11,20 +13,23 @@ public class PostResponse {
     private final String title;
     private final String content;
     private final String nickname;
+    private final LocalDateTime create_at;
 
     public PostResponse(Post post) {
         this.id = post.getId();
         this.title = post.getTitle();
         this.content = post.getContent();
+        this.create_at = post.getCreatedAt();
         this.nickname = post.getMember().getNickname();
     }
 
     @Builder
-    public PostResponse(Long id, String title, String content, String nickname) {
+    public PostResponse(Long id, String title, String content, String nickname, LocalDateTime create_at) {
         this.id = id;
         this.title = title;
         this.content = content;
         this.nickname = nickname;
+        this.create_at = create_at;
     }
 
 }
