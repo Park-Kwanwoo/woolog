01e1fd4b6ba82648bb0dcba46805409ee1ade011
박관우 <pgw4712@icloud.com>
2025-01-16 02:33:11 +0900

    admin 권한 판단 변수 추가

--- src/main/java/com/woolog/response/MemberResponse.java
+++ src/main/java/com/woolog/response/MemberResponse.java
@@ -1,6 +1,7 @@
 package com.woolog.response;
 
 import com.woolog.domain.Member;
+import com.woolog.domain.Role;
 import lombok.Builder;
 import lombok.Getter;
 
@@ -10,12 +11,14 @@ public class MemberResponse {
     private final String email;
     private final String name;
     private final String nickname;
+    private final boolean isAdmin;
 
     @Builder
-    public MemberResponse(String email, String name, String nickname) {
+    public MemberResponse(String email, String name, String nickname, boolean isAdmin) {
         this.email = email;
         this.name = name;
         this.nickname = nickname;
+        this.isAdmin = isAdmin;
     }
 
     public static MemberResponse of(Member member) {
@@ -23,6 +26,7 @@ public class MemberResponse {
                 .email(member.getEmail())
                 .name(member.getName())
                 .nickname(member.getNickname())
+                .isAdmin(member.getRole().equals(Role.ADMIN))
                 .build();
     }
 }
