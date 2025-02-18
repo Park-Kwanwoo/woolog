4e04af5a69a36ea77490dd0c7f52890136b86369
박관우 <pgw4712@icloud.com>
2025-02-18 20:04:14 +0900

    마이페이지 뷰 추가

--- front/src/router/index.ts
+++ front/src/router/index.ts
@@ -1,9 +1,10 @@
-import { createRouter, createWebHistory } from 'vue-router'
+import {createRouter, createWebHistory} from 'vue-router'
 import HomeView from '@/views/HomeView.vue'
-import WriteView from "@/views/WriteView.vue";
-import ReadView from "@/views/ReadView.vue";
-import EditView from "@/views/EditView.vue";
+import WriteView from "@/views/post/WriteView.vue";
+import ReadView from "@/views/post/ReadView.vue";
+import EditView from "@/views/post/EditView.vue";
 import LoginView from "@/views/LoginView.vue";
+import MyPage from "@/views/member/MyPage.vue";
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -35,6 +36,11 @@ const router = createRouter({
       name: "login",
       component: LoginView,
       props: true
+    },
+    {
+      path: "/mypage",
+      name: "mypage",
+      component: MyPage
     }
   ],
 })
