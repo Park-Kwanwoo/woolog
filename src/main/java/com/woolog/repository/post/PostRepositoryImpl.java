cdb81b557a68b562cbfdd59c8233452e99988567
박관우 <pgw4712@icloud.com>
2025-03-01 23:43:34 +0900

    검색어 조건 추가

--- src/main/java/com/woolog/repository/post/PostRepositoryImpl.java
+++ src/main/java/com/woolog/repository/post/PostRepositoryImpl.java
@@ -1,10 +1,14 @@
 package com.woolog.repository.post;
 
+import com.querydsl.core.types.Expression;
+import com.querydsl.core.types.dsl.BooleanExpression;
+import com.querydsl.core.types.dsl.Expressions;
 import com.querydsl.jpa.impl.JPAQueryFactory;
 import com.woolog.domain.Post;
 import com.woolog.request.post.PagingRequest;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.PageImpl;
+import org.springframework.util.StringUtils;
 
 import java.util.List;
 
@@ -20,14 +24,25 @@ public class PostRepositoryImpl implements PostRepositoryCustom {
 
         Long totalCount = jpaQueryFactory.select(post.count())
                 .from(post)
+                .where(containsKeyword(pagingRequest.getKeyword()))
                 .fetchFirst();
 
         List<Post> items = jpaQueryFactory.selectFrom(post)
                 .limit(pagingRequest.getSize())
                 .offset(pagingRequest.getOffset())
+                .where(containsKeyword(pagingRequest.getKeyword()))
+                .on()
                 .orderBy(post.id.desc())
                 .fetch();
 
-        return new PageImpl<Post>(items, pagingRequest.getPageable(), totalCount);
+        return new PageImpl<>(items, pagingRequest.getPageable(), totalCount);
+    }
+
+    private BooleanExpression containsKeyword(String keyword) {
+
+        if (StringUtils.hasText(keyword)) {
+            return post.title.contains(keyword);
+        }
+        return null;
     }
 }
